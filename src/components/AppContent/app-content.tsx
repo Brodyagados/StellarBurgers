1bb49d35442ff8800ad3257dc8e39f506532a4ed
Igor Mezentsev <schumm@mail.ru>
2024-08-31 18:06:22 +0500

    Добавлена компонента "BurgerConstructor"

--- src/components/AppContent/app-content.tsx
+++ src/components/AppContent/app-content.tsx
@@ -1,9 +1,11 @@
 import styles from './app-content.module.css';
+import { BurgerConstructor } from './BurgerConstructor';
 import { BurgerIngredients } from './BurgerIngredients';
 
 const AppContent = () => (
   <div className={styles.container}>
     <BurgerIngredients />
+    <BurgerConstructor />
   </div>
 );
 
