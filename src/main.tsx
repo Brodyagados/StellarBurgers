ec3a06f9ae7357bd1cda32208e84f61d3a8bd066
Igor Mezentsev <schumm@mail.ru>
2024-09-12 10:39:12 +0500

    Подключен Redux DevTools

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import { App } from './components';
 import { createStore } from 'redux';
 import { rootReducer } from './services/reducers';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, {}, composeWithDevTools());
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
