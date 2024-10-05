ed0f41110116343daf910e0776b83a47e0647270
Igor Mezentsev <schumm@mail.ru>
2024-10-05 17:35:14 +0500

    Страница "Регистрация"

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { getIngredientsList } from '../../services/ingredients-list/actions';
-import { HomePage, LoginPage } from '../../pages';
+import { HomePage, LoginPage, RegisterPage } from '../../pages';
 
 function App() {
   const dispatch = useDispatch();
@@ -18,6 +18,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/register/register-page.tsx
@@ -0,0 +1,49 @@
+import { Button, EmailInput, Input } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ChangeEvent, useCallback, useState } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './register-page.module.css';
+
+type TPasswordInputType = 'password' | 'text';
+
+const LoginPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordInputType, setPasswordInputType] = useState<TPasswordInputType>('password');
+
+  const onNameChange = (e: ChangeEvent<HTMLInputElement>) => setName(e.target.value);
+  const onEmailChange = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
+  const onPasswordChange = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
+
+  const onPasswordShowClick = useCallback(
+    () => setPasswordInputType(passwordInputType === 'password' ? 'text' : 'password'),
+    [passwordInputType]
+  );
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.form}>
+        <span className='text text_type_main-medium'>Регистрация</span>
+        <Input name='name' value={name} placeholder='Имя' onChange={onNameChange} />
+        <EmailInput name='email' value={email} onChange={onEmailChange} />
+        <Input
+          name='password'
+          value={password}
+          type={passwordInputType}
+          placeholder='Пароль'
+          onChange={onPasswordChange}
+          icon={passwordInputType === 'password' ? 'ShowIcon' : 'HideIcon'}
+          onIconClick={onPasswordShowClick}
+        />
+        <Button htmlType='button'>Зарегистрироваться</Button>
+      </div>
+      <div className={styles.links}>
+        <span className='text text_type_main-default text_color_inactive'>
+          Уже зарегистрированы? <Link to='/login'>Войти</Link>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
