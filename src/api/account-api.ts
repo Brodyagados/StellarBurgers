40df904e06a076ad375556ef5feb1f84bd116f8e
Igor Mezentsev <schumm@mail.ru>
2024-10-05 20:01:05 +0500

    Добавлен Api-метод восстановления пароля

--- src/api/account-api.ts
+++ src/api/account-api.ts
@@ -4,4 +4,7 @@ import apiClient from '../utils/api-client';
 export class AccountApi {
   static sendTokenForResetPassword = (data: TSendTokenForResetPasswordDto) =>
     apiClient.request<TResetPasswordModel>('/password-reset', { method: 'post', body: JSON.stringify(data) });
+
+  static resetPassword = (data: TResetPasswordDto) =>
+    apiClient.request<TResetPasswordModel>('/password-reset/reset', { method: 'post', body: JSON.stringify(data) });
 }
--- src/models/account-model.ts
+++ src/models/account-model.ts
@@ -2,6 +2,11 @@ export type TSendTokenForResetPasswordDto = {
   email: string;
 };
 
+export type TResetPasswordDto = {
+  password: string;
+  token: string;
+};
+
 export type TResetPasswordModel = {
   success: boolean;
   message: string;
