ed8137677ec7e0f869d835aaeadba9ef9253dd42
Igor Mezentsev <schumm@mail.ru>
2024-10-06 13:29:31 +0500

    Добавлен выход из системы

--- src/layouts/profile/profile-layout.tsx
+++ src/layouts/profile/profile-layout.tsx
@@ -1,21 +1,37 @@
 import styles from './profile-layout.module.css';
 import { routes } from '../../utils/constants';
 import { ProfileNavigateItem } from '../../components';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
+import { AccountApi } from '../../api';
 
-const ProfileLayout = () => (
-  <div className={styles.container}>
-    <div className={styles.content}>
-      <div className={styles.left}>
-        <div className={styles.links}>
-          <ProfileNavigateItem to={routes.PROFILE} text='Профиль' />
-          <ProfileNavigateItem to={routes.PROFILE_ORDERS} text='История заказов' />
-          <ProfileNavigateItem to='/exit' text='Выход' />
+const ProfileLayout = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = useCallback(async () => {
+    const { success } = await AccountApi.logout();
+
+    if (success) {
+      navigate(routes.LOGIN);
+    }
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.left}>
+          <div className={styles.links}>
+            <ProfileNavigateItem to={routes.PROFILE} text='Профиль' />
+            <ProfileNavigateItem to={routes.PROFILE_ORDERS} text='История заказов' />
+            <span className='text text_type_main-medium text_color_inactive' onClick={handleLogout}>
+              Выход
+            </span>
+          </div>
         </div>
+        <Outlet />
       </div>
-      <Outlet />
     </div>
-  </div>
-);
+  );
+};
 
 export default ProfileLayout;
