506da7bda5cca0413c81592e79f31111f7515372
Igor Mezentsev <schumm@mail.ru>
2024-10-31 19:53:57 +0500

    Страница "Информация о заказе" (с тестовыми данными)

--- src/components/order-information/order-information.tsx
+++ src/components/order-information/order-information.tsx
@@ -24,27 +24,52 @@ const order: TOrderModel = {
   number: 58132
 };
 
+type TUniqueIngredient = {
+  ingredient: TIngredientModel;
+  count: number;
+};
+
 const OrderInformation = () => {
   const { number, createdAt, name, ingredients, status } = order;
   const ingredientsInfo = useSelector((store) => getIngredientsByIdsSelector(store, ingredients));
   const statusMetadata = orderStatusMetadata[status];
 
-  const total = useMemo(
-    () => ingredientsInfo.reduce((result: TIngredientModel['price'], ingredient) => result + ingredient.price, 0),
+  const { uniqueIngredients, total } = useMemo(
+    () =>
+      ingredientsInfo.reduce(
+        (result: { uniqueIngredients: Map<string, TUniqueIngredient>; total: TIngredientModel['price'] }, ingredient) => ({
+          uniqueIngredients: result.uniqueIngredients.set(ingredient._id, {
+            ingredient,
+            count: result.uniqueIngredients.has(ingredient._id) ? result.uniqueIngredients.get(ingredient._id)!.count + 1 : 1
+          }),
+          total: result.total + ingredient.price
+        }),
+        {
+          uniqueIngredients: new Map(),
+          total: 0
+        }
+      ),
     [ingredientsInfo]
   );
 
   return (
     <div className={`p-6 ${styles.container}`}>
-      <p className='text text_type_digits-default'>{`#${number}`}</p>
+      <p className={`text text_type_digits-default ${styles.number}`}>{`#${number}`}</p>
       <p className='pt-10 text text_type_main-medium'>{name}</p>
       <p className='pt-3 text text_type_main-small' style={{ color: statusMetadata.color }}>
         {statusMetadata.description}
       </p>
-      <p className='pt-15 text text_type_main-medium'>Состав</p>
+      <p className='pt-15 text text_type_main-medium'>Состав:</p>
       <div className={`pt-6 ${styles.ingredients}`}>
-        {ingredientsInfo.map((ingredient) => (
-          <img src={ingredient.image} />
+        {Array.from(uniqueIngredients).map(([_, { ingredient, count }]) => (
+          <div className={styles.ingredient}>
+            <img className={styles.icon} src={ingredient.image} />
+            <span className='pl-4 text text_type_main-small'>{ingredient.name}</span>
+            <div className={`pl-4 ${styles.total}`}>
+              <span className='text text_type_digits-default'>{`${count} x ${ingredient.price}`}</span>
+              <CurrencyIcon type='primary' />
+            </div>
+          </div>
         ))}
       </div>
       <div className={`pt-10 ${styles.footer}`}>
