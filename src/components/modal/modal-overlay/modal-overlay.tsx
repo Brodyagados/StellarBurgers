b24ae7c220fc77c8db5ddcc5bbdc281ff55d4877
Igor Mezentsev <schumm@mail.ru>
2024-11-07 20:40:44 +0500

    Updates

--- src/components/modal/modal-overlay/modal-overlay.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './modal-overlay.module.css';
-
-type ModalOverlayProps = {
-  onClick: () => void;
-};
-
-const ModalOverlay = ({ onClick }: ModalOverlayProps) => <div className={styles.container} onClick={onClick}></div>;
-
-export default ModalOverlay;
